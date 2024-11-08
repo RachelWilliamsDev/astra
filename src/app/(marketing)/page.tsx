2aa4db3e7b57742cc68a9c56a10c1c1d5da1778e
RachelWilliamsDev <Rachel@stacktiger.co>
2024-11-08 16:12:41 +0000

    feat: home page how it works section

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,6 +1,8 @@
 import { Container, Wrapper } from "@/components";
 import { BorderBeam } from "@/components/ui/border-beam";
 import { Button } from "@/components/ui/button";
+import SectionBadge from "@/components/ui/section-badge";
+import { perks } from "@/constants";
 import { ArrowRight, ChevronRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -69,6 +71,40 @@ const HomePage = () => {
                 />
                 <BorderBeam size={250} duration={12} delay={9} />
               </div>
+            </div>
+          </div>
+        </Container>
+      </Wrapper>
+      {/* How it works */}
+      <Wrapper className="flex flex-col items-center justify-center py-12 relative">
+        <Container>
+          <div className="max-w-md mx-auto text-start md:text-center">
+            <SectionBadge title="The Process" />
+            <h2 className="text-3xl lg:text-4xl font-semibold my-6">
+              Three steps to build your dream website
+            </h2>
+            <p className="text-muted-foreground mt-6">
+              Turn your vision into reality in just 3 simple steps
+            </p>
+          </div>
+        </Container>
+        <Container>
+          <div className="flex flex-col items-center justify-center py-10 md:py-20 w-full">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full divide-x-0 md:divide-x divide-y md:divide-y-0 divide-gray-900 first:border-none first:border-gray-900">
+              {perks.map((perk, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col items-start px-4 py-4 md:px-6 lg:px-8 lg:py-6"
+                >
+                  <div className="flex items-center justify-center">
+                    <perk.icon className="w-8 h-8" />
+                  </div>
+                  <h3 className="text-lg font-medium mt-4">{perk.title}</h3>
+                  <p className="text-muted-foreground mt-2 text-start">
+                    {perk.info}
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
         </Container>
