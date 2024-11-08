c531b87b78372a0c0de97f4532922d0357a48a77
Rachel Williams <83422650+RachelWilliamsDev@users.noreply.github.com>
2024-11-08 15:42:31 +0000

    feat: home page hero section (#2)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
+import Container from "./global/container";
 import Icons from "./global/icons";
+import Wrapper from "./global/wrapper";
 import Navbar from "./navigation/navbar";
 
-export { Icons, Navbar };
+export { Container, Icons, Navbar, Wrapper };
